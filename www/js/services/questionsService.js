cd518b8c2721ccb97a100c3f12ccdafb596e5e45
YmBIgo <IgYmB0218LP@gmail.com>
2016-07-13 08:40:01 +0900

    finish basic

---
+++ www/js/services/questionsService.js
@@ -0,0 +1,27 @@
+// This is a JavaScript file
+
+app.value('questionsService', {
+    questions : [
+        {
+            "text":"福沢諭吉の身長は？",
+            "answer":"173cm",
+            "choices":["165cm", "168cm", "179cm"]
+        },
+        {
+            "text":"福沢諭吉が留学に訪れた大学はどこ？",
+            "answer":"ブラウン大学",
+            "choices":["コーネル大学","コロンビア大学","ペンシルベニア大学"]
+        },
+        {
+            "text":"いつから一万円紙幣表面の肖像に採用されたのか？",
+            "answer":"1984年",
+            "choices":["1974年","1994年","2004年"]
+        },
+        {
+            "text":"創刊した新聞社の名前は？",
+            "answer":"時事新報",
+            "choices":["朝日新聞","日報社","京城日報社"]
+        }
+        ]
+
+});
